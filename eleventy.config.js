0fa886ed7ce9b2c7d4e6180a16333d55f3a315b3
tettehseth67 <68742079+tettehseth67@users.noreply.github.com>
2026-06-20 00:01:05 -0400

    update:commit all

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,6 @@ module.exports = function (eleventyConfigure) {
     // Asset Passthrough Engines
     eleventyConfigure.addPassthroughCopy("src/css");
     eleventyConfigure.addPassthroughCopy("src/js");
-
-    // New: Automated media, document, and structural assets tracking
     eleventyConfigure.addPassthroughCopy("src/assets/images");
     eleventyConfigure.addPassthroughCopy("src/assets/docs");
 
